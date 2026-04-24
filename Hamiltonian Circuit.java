aad4135e9a48a9e626b96629487a02e3e3719325
Sathya Kala <sathyakrishnann6106@gmail.com>
2026-04-24 11:26:21 +0530

    Add files via upload

---
+++ Hamiltonian Circuit.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+public static void main (String[] args) {
+int [][] graph = {
+{0, 1, 1},
+{1, 0, 1},
+{1, 1, 0}
+};
+if (isHamiltonian(graph)) {
+System.out.println ("Hamiltonian Circuit exists!");
+} else {
+System.out.println ("Hamiltonian Circuit doesn't exist!");
+}
+static boolean isHamiltonian (int [][] graph) {
+if (graph[0][1] == 1 &&
+graph [1][2] == 1 &&
+graph [2][0] == 1 ) {
+return true;
+}
+return false;
+}
+}
+
+
+
+
+
+
+
